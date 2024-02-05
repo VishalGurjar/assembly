a84b87c2fc043b0a7e58898769a9bb0b15b91212
VishalGurjar <95881264+VishalGurjar@users.noreply.github.com>
2024-02-06 01:14:17 +0530

    Add files via upload

---
+++ src/components/Dashboard.js
@@ -0,0 +1,39 @@
+import React from "react";
+// import { Helmet } from "react-helmet";
+// import brand from "../../../assets/dummy/brand";
+// import { PapperBlock } from "../../../components";
+// import CompossedLineBarArea from "./CompossedLineBarArea";
+// import StrippedTable from "../Table/StrippedTable";
+// import TableViewIcon from "@mui/icons-material/TableView";
+// import PageTitle from "src/components/PageTitle";
+// import { styled } from "@mui/material/styles";
+
+function BasicTable() {
+  return (
+    <div title="Dashboard">
+     vishal gurjar
+      {/* <PapperBlock
+        title="Statistic Chart"
+        icon={<TableViewIcon />}
+        desc=""
+        overflowX
+      >
+        <div>
+          <CompossedLineBarArea />
+        </div>
+      </PapperBlock> */}
+      {/* <PapperBlock
+        title="Table"
+        whiteBg
+        icon={<TableViewIcon />}
+        desc="UI Table when no data to be shown"
+      >
+        <div>
+          <StrippedTable />
+        </div>
+      </PapperBlock> */}
+    </div>
+  );
+}
+
+export default BasicTable;
